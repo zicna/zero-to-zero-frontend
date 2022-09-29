3425bc3e28a308ffc5f872d217244a31b713c5f1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-29 11:08:12 -0500

    wrap index.js file in context provider so context can be avaiable anywhere in the application

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App'
 
 import { BrowserRouter } from 'react-router-dom'
 
+import { AuthContextProvider } from './store/auth-context'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 )
