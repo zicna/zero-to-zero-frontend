12c3145061bcb445f72c534fcb3f350c99bb2b87
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-29 15:00:14 -0500

    cleanup routes container

--- src/routes container/MainRoutesContainer.js
+++ src/routes container/MainRoutesContainer.js
@@ -6,19 +6,13 @@ import Home from '../pages/Home'
 import Profile from '../pages/Profile.js'
 import PageNotFound from '../pages/PageNotFound'
 
-export default function MainRoutesContainer({ setIsLoggedIn }) {
+export default function MainRoutesContainer() {
   return (
     <>
       <Routes>
         <Route path="/home" element={<Home />}></Route>
-        <Route
-          path="/auth"
-          element={<Auth setIsLoggedIn={setIsLoggedIn} />}
-        ></Route>
-        <Route
-          path="/logout"
-          element={<LogOut setIsLoggedIn={setIsLoggedIn} />}
-        />
+        <Route path="/auth" element={<Auth />}></Route>
+        <Route path="/logout" element={<LogOut />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
