438fc7bb636aa8ffe31a8104d37ffe6901800293
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-30 15:43:49 -0500

    connect app to redux and provide a store to index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 import { AuthContextProvider } from './store/auth-context'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <AuthContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AuthContextProvider>
+    </Provider>
   </React.StrictMode>
 )
