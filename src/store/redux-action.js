637de68858de08fe44b2cbb251434da01066bb2f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-30 15:44:28 -0500

    extract action in separate file to avoid typos

---
+++ src/store/redux-action.js
@@ -0,0 +1,7 @@
+export const LOGIN = "login";
+export const LOGOUT = "logout" ;
+export const SIGNUP= "signup";
+export const CLEAR = "clear"
+// export const = ;
+// export const = ;
+// export const = ;
