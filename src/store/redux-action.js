657dc7f30495fccb34fcb85f13521aee4c0e2d32
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-10-04 15:28:00 -0500

    add new action 'LOAD'- used across multiple reducers

--- src/store/redux-action.js
+++ src/store/redux-action.js
@@ -1,8 +1,8 @@
+export const LOAD = 'load'
 export const LOGIN = 'login'
 export const LOGOUT = 'logout'
 export const SIGNUP = 'signup'
 export const CLEAR = 'clear'
-export const LOADDATA = 'load all data'
 // TODO: use ENUM to store all constants 
 // export const = ;
 // export const = ;
