0a1c39f3ee06047055657a76ea21b0985e4c6bf9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-11-04 16:00:14 -0500

    add wrapper component that will render 'children' component if 'isLoggedIn' from AuthContex is true

---
+++ src/components/Wapper/PermittedWrapper.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+import { useDispatch } from 'react-redux'
+import { Navigate } from 'react-router'
+// import Home from "../../pages/Home";
+import AuthContext from '../../store/auth-context'
+import { CLEAR, MUST_ACTION } from '../../store/redux-action'
+
+const PermittedWrapepr = ({ children }) => {
+  // * dip into context to see if user is logged in
+  const authCtx = useContext(AuthContext)
+  const isLoggedIn = authCtx.isLoggedIn
+
+  const dispatch = useDispatch()
+
+  // * guard clause to check if user is logged in
+  // ! problem: not just to return Home component but also to redirect to "/home" URL
+  // if(!isLoggedIn) return < Home/>
+  // * solution: redirect to "/auth" path and display message to access "/profile" you must be "signed in" or "logged in"
+  if (!isLoggedIn) {
+    dispatch({type: MUST_ACTION})
+    // ! this should be resolved with redux thunk
+    setTimeout(() => {
+        dispatch({ type: CLEAR })
+      }, 3000)
+    return <Navigate to="/auth" />
+  }
+
+  return { children }
+}
+
+export default PermittedWrapepr
